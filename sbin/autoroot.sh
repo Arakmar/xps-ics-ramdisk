4ec2638309f3c8c654c772b20de924a326c24cce
Yoann Laissus <yoann.laissus@gmail.com>
2012-06-26 20:25:50 +0200

    Improved autoroot script. It now copy missing files even if /system/autorooted already exists.

--- sbin/autoroot.sh
+++ sbin/autoroot.sh
@@ -2,63 +2,62 @@
 
 # DooMLoRD: autoroot script (v13)
 
+export PATH=/sbin
+
 # [START] setting up
 echo "[START] remounting system" > /data/local/tmp/autorootlog.txt
-/sbin/busybox mount -o remount,rw /system >> /data/local/tmp/autorootlog.txt
+mount -o remount,rw /system >> /data/local/tmp/autorootlog.txt
 
 # [CHECK] searching if autoroot was done before
 echo "[CHECK] searching for autorooted file " >> /data/local/tmp/autorootlog.txt
-if /sbin/busybox [ ! -f /system/autorooted ];
-then
-echo "[FOUND] autorooted file " >> /data/local/tmp/autorootlog.txt
+if [ ! -f /system/autorooted ]; then
+	echo "[NOT FOUND] autorooted file not found, removing existing root files ... " >> /data/local/tmp/autorootlog.txt
+	rm /system/bin/su
+	rm /system/xbin/su
+	rm /system/app/Superuser.apk
+	rm /data/app/Superuser.apk
+	rm /system/xbin/busybox
+	rm /system/bin/busybox
+fi
 
 # [CHECK] verify /system/xbin
 echo "[CHECK] verifying /system/xbin " >> /data/local/tmp/autorootlog.txt
-/sbin/busybox mkdir /system/xbin
-/sbin/busybox chmod 755 /system/xbin
+mkdir /system/xbin
+chmod 755 /system/xbin
 
 # [SU binary] remove existing occurances and push su
-echo "[SU binary] remove existing occurances and push su" >> /data/local/tmp/autorootlog.txt
-/sbin/busybox rm /system/bin/su
-/sbin/busybox rm /system/xbin/su
-/sbin/busybox cp /res/autoroot/su /system/xbin/su
-/sbin/busybox chown root.root /system/xbin/su
-/sbin/busybox chmod 06755 /system/xbin/su
+if [ ! -f /system/xbin/su ]; then
+	echo "[SU binary] pushing su ..." >> /data/local/tmp/autorootlog.txt
+	cp /res/autoroot/su /system/xbin/su
+fi
+echo "[SU binary] fixing su perms and owners ..." >> /data/local/tmp/autorootlog.txt
+chown root.root /system/xbin/su
+chmod 06755 /system/xbin/su
 
 # [Superuser app] remove existing occurances and push app
-echo "[Superuser app] remove existing occurances and push app" >> /data/local/tmp/autorootlog.txt
-/sbin/busybox rm /system/app/Superuser.apk
-/sbin/busybox rm /data/app/Superuser.apk
-/sbin/busybox cp /res/autoroot/Superuser.apk /system/app/Superuser.apk
-/sbin/busybox chown root.root /system/app/Superuser.apk
-/sbin/busybox chmod 0644 /system/app/Superuser.apk
+
+if [ ! -f /system/app/Superuser.apk ]; then
+	echo "[Superuser app] pushing app ..." >> /data/local/tmp/autorootlog.txt
+	cp /res/autoroot/Superuser.apk /system/app/Superuser.apk
+fi
+echo "[Superuser app] fixing su perms and owners ..." >> /data/local/tmp/autorootlog.txt
+chown root.root /system/app/Superuser.apk
+chmod 0644 /system/app/Superuser.apk
 
 # [busybox binary] remove existing occurances and push busybox
-echo "[busybox binary] remove existing occurances and push busybox" >> /data/local/tmp/autorootlog.txt
-if /sbin/busybox [ ! -f /system/xbin/busybox ];
+if [ ! -f /system/xbin/busybox ];
 then
-echo "[busybox binary] not found in /system/xbin/busybox " >> /data/local/tmp/autorootlog.txt
-if /sbin/busybox [ ! -f /system/bin/busybox ];
-then
-echo "[busybox binary] not found in /system/bin/busybox " >> /data/local/tmp/autorootlog.txt
-/sbin/busybox cp /res/autoroot/busybox /system/xbin/busybox
-/sbin/busybox chown root.root /system/xbin/busybox
-/sbin/busybox chmod 4777 /system/xbin/busybox
+	echo "[busybox binary] pushing busybox ..." >> /data/local/tmp/autorootlog.txt
+	cp /res/autoroot/busybox /system/xbin/busybox
+fi
+echo "[busybox] fixing busybox perms and owners ..." >> /data/local/tmp/autorootlog.txt
+chown root.root /system/xbin/busybox
+chmod 4777 /system/xbin/busybox
 /system/xbin/busybox --install -s /system/xbin/
-fi
-fi
 
 # [DONE] placing flag
 echo "[DONE] placing flag" >> /data/local/tmp/autorootlog.txt
-/sbin/busybox touch /system/autorooted
-
-else
-
-echo "[FOUND] autorooted file" >> /data/local/tmp/autorootlog.txt
-echo "Device has been already autorooted" >> /data/local/tmp/autorootlog.txt
-echo "nothing to do... bye bye..." >> /data/local/tmp/autorootlog.txt
-
-fi;
+touch /system/autorooted
 
 # [DONE] all done exiting
 echo "[DONE] all done exiting" >> /data/local/tmp/autorootlog.txt 
